21c02605df23a9e0c7ac3673789b1ea68765ae12
230660221092 <230660221092@student.unsap.ac.id>
2025-06-21 16:49:14 +0700

    menambahkan latihan react dengan vite

---
+++ 230660221092/praktikum-react/latihan-react-vite/src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+
+import './App.css'; // pastikan file CSS diimpor
+
+function App() {
+  return (
+    <div>
+      <h1 className="title">Cecilia Erisca</h1>
+      <p className="intro">
+        Saya adalah mahasiswa Sistem Informasi dari Fakultas Ilmu Komputer yang antusias dalam teknologi dan pengembangan web.
+      </p>
+    </div>
+  );
+}
+
+export default App;
